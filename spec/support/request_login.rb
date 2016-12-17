b7ce16b7afbbc21c20bc6f6906a6267d50409d2f
HEROGWP <pp820819@gmail.com>
2016-12-17 15:46:41 +0800

    rspec login

--- spec/support/request_login.rb
+++ spec/support/request_login.rb
@@ -1,13 +1,8 @@
 module RequestLogin
 
-    def login_user
-      @user ||= FactoryGirl.create(:user)
-      post_via_redirect user_session_path, params:{ 'user[email]' => @user.email, 'user[password]' => "12345678"}
-    end
-
-    def login_admin
-      @user ||= FactoryGirl.create(:admin)
-      post_via_redirect user_session_path, params:{ 'user[email]' => @user.email, 'user[password]' => "12345678"}
-    end
+  def login_user(user = nil)
+    user ||= FactoryGirl.create(:user)
+    post_via_redirect user_session_path, { 'user[email]' => user.email, 'user[password]' => "12345678"}
+  end
 
 end
